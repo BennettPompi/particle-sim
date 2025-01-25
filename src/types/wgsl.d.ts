6d54ed739104308b5b410ddb31bdd6fb433ed96f
BennettPompi <bennettp2002@gmail.com>
2025-01-25 16:46:14 -0500

    this seems to make my imports work

--- src/types/wgsl.d.ts
+++ src/types/wgsl.d.ts
@@ -1,5 +1,5 @@
-// filepath: /path/to/src/types/wgsl.d.ts
-// declare module "*.wgsl" {
-//     const content: string;
-//     export default content;
-// }
+filepath: /path/ot / src / types / wgsl.d.ts;
+declare module "*.wgsl" {
+    const content: string;
+    export default content;
+}
